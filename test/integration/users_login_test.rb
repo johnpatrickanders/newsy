2d343461dfebb0838ce51e84d9152c1c6e34e422
johnsimplepractice <john.anders@simplepractice.com>
2024-03-29 10:31:32 -0400

    paginate users and test

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -111,4 +111,21 @@ class RememberingTest < UsersLogin
     log_in_as(@user, remember_me: '0')
     assert cookies[:remember_token].blank?
   end
+
+  test "login and access all layouts" do
+    get root_path
+    assert_template 'static_pages/home'
+    assert_select "a[href=?]", root_path, count: 2
+    assert_select "a[href=?]", help_path
+    assert_select "a[href=?]", about_path
+    assert_select "a[href=?]", contact_path
+    assert_select "a[href=?]", login_path
+    assert_select "a[href=?]", users_path, false
+    log_in_as(@user)
+    get root_path
+    assert_select "a[href=?]", logout_path
+    assert_select "a[href=?]", users_path
+    assert_select "a[href=?]", user_path(@user)
+    assert_select "a[href=?]", edit_user_path(@user)
+  end
 end
