947401a92d570f7c2bcf2405d2e8df9d4da228dd
John Anders <johnpatrickanders@gmail.com>
2024-04-05 14:32:22 -0400

    add initial article form

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,36 @@
+class ArticlesController < ApplicationController
+  before_action :logged_in_user, only: [:create, :destroy]
+  before_action :correct_user,   only: :destroy
+
+  def create
+    @article = current_user.articles.build(article_params)
+
+    # hard code source until source feature built
+    @article.source_id = 1
+
+    if @article.save
+      flash[:success] = "Article posted!"
+      redirect_to root_url
+    else
+      @feed_items = current_user.feed.paginate(page: 1)
+      render 'static_pages/home', status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @article.destroy
+    flash[:success] = "Article deleted"
+    redirect_back_or_to(root_url, status: :see_other) # equivalent to commented out code above
+  end
+
+  private
+
+    def article_params
+      params.require(:article).permit(:content, :source_id, :url, :title)
+    end
+
+    def correct_user
+      @article = current_user.articles.find_by(id: params[:id])
+      redirect_to root_url, status: :see_other if @article.nil?
+    end
+end
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,7 +1,8 @@
 class Article < ApplicationRecord
   belongs_to :user
-  belongs_to :source
+  # belongs_to :source
   default_scope -> { order(created_at: :desc) }
   validates :user_id, presence: true
+  validates :title, presence: true
   validates :source_id, presence: true
 end
